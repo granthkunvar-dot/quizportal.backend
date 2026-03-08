de037edc288a411383a6292bc6a99546d79960c4
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-08 15:21:09 +0530

    Update followRoutes.js

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const asyncHandler = require("../utils/asyncHandler");
-const { requireAuth } = require("../middlewares/authMiddleware");
+const { requireAuth } = require("../middleware/authMiddleware");
 const { followUser, unfollowUser, getFollowers, getFollowing, getPublicProfile } = require("../controllers/followController");
 
 const router = express.Router();
@@ -12,3 +12,4 @@ router.get("/:id/followers", asyncHandler(getFollowers));
 router.get("/:id/following", asyncHandler(getFollowing));
 
 module.exports = router;
+
