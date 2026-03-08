1be987067f9abb99f445f58e14869010269a4f9d
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-08 17:17:17 +0530

    Update chatHandler.js

--- controllers/chatHandler.js
+++ controllers/chatHandler.js
@@ -1,4 +1,4 @@
-const { pool } = require("../config/db");
+const pool = require("../config/db");
 
 const initSocket = (io, sessionMiddleware) => {
     // 1. Wrap Express session middleware for Socket.io
@@ -110,5 +110,6 @@ const initSocket = (io, sessionMiddleware) => {
 };
 
 module.exports = { initSocket };
+
 
 
