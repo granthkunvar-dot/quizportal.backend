eaf3f351fad0a29bd6e5b31f7a38c7262b7498b0
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-08 21:05:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
+const mysql = require("mysql2/promise");
+
+console.log("=== DB CONFIG ===");
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
-const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
