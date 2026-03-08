b463c272407111171507341d597a4938dbb031ca
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-08 16:48:50 +0530

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,4 @@
-const { pool } = require("../db");
+const { pool } = require("../config/db");
 const { closeSeasonIfExpired, takeDailyRankSnapshot } = require("../services/seasonService");
 const { evaluateAchievements } = require("../services/achievementService");
 
