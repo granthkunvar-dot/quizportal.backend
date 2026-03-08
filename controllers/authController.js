51110026964f4df34c635d78ef37d330511bd149
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-08 16:46:34 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const { pool } = require("../db");
+const { pool } = require("../config/db");
 
 const SALT_ROUNDS = 10;
 const normalizeEmail = (email) => String(email || "").trim().toLowerCase();
