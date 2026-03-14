b39c2e791ca154fe20e65989fbf1f4212e02bd93
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-14 14:36:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const sessionMiddleware = require("./config/session");
 const authRoutes = require("./routes/authRoutes");
 const studentRoutes = require("./routes/studentRoutes");
-const adminAuthRoutes = require("./routes/adminAuth");
 const adminRoutes = require("./routes/adminRoutes");
 const followRoutes = require("./routes/followRoutes");
 const instructorRoutes = require("./routes/instructorRoutes");
@@ -28,7 +27,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/student", studentRoutes);
-app.use("/api/admin/auth", adminAuthRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/follow", followRoutes);
 app.use("/api/instructor", instructorRoutes);
