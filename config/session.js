5d4d62053cf689a380aa1830b0febd95ef479834
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-14 15:53:38 +0530

    Update session.js

--- config/session.js
+++ config/session.js
@@ -1,15 +1,8 @@
 const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-
-// Reuse the same pool instead of opening new connections
-const pool = require("./db");
-
-const sessionStore = new MySQLStore({}, pool);
 
 const sessionMiddleware = session({
   name: "quizportal.sid",
   secret: process.env.SESSION_SECRET || "dev-secret",
-  store: sessionStore,
   resave: false,
   saveUninitialized: false,
   cookie: {
