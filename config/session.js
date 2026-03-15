1cd1c735eb0093b6b90e37549d2b6c2b5c0ceb88
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-15 14:36:05 +0530

    Update session.js

--- config/session.js
+++ config/session.js
@@ -1,8 +1,16 @@
 const session = require("express-session");
+const { Redis } = require("@upstash/redis");
+const RedisStore = require("connect-redis").default;
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 const sessionMiddleware = session({
   name: "quizportal.sid",
   secret: process.env.SESSION_SECRET || "dev-secret",
+  store: new RedisStore({ client: redis }),
   resave: false,
   saveUninitialized: false,
   cookie: {
