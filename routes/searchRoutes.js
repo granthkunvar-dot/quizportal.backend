496be31b1d5fa05e19f6aed333ff293b291495b2
granthkunvar-dot <granth.kunvar@gmail.com>
2026-03-21 20:22:59 +0530

    Update searchRoutes.js

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const asyncHandler = require("../middleware/asyncHandler");
-const { searchUsers, getPublicProfile } = require("../controllers/searchController");
+const { searchUsers } = require("../controllers/searchController");
+
 const router = express.Router();
 
 router.get("/search", asyncHandler(searchUsers));
-router.get("/profile/:displayName", asyncHandler(getPublicProfile));
 
 module.exports = router;
